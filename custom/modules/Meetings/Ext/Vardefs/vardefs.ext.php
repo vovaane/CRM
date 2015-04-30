5531642dc13a185f81046f30460c8d7ce621bf2c
Vovaane <mihalev@lookatme.ru>
2015-04-30 13:30:25 +0400

    ehd

---
+++ custom/modules/Meetings/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,40 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+ // created: 2014-01-20 12:22:32
+
+ 
+
+
+
+$dictionary['Meeting']['fields']['SecurityGroups'] = array (
+  	'name' => 'SecurityGroups',
+    'type' => 'link',
+	'relationship' => 'securitygroups_meetings',
+	'module'=>'SecurityGroups',
+	'bean_name'=>'SecurityGroup',
+    'source'=>'non-db',
+	'vname'=>'LBL_SECURITYGROUPS',
+);
+
+
+
+
+
+
+ // created: 2014-01-20 12:22:32
+
+ 
+
+
+ // created: 2014-01-20 12:22:32
+
+ 
+
+
+ // created: 2014-01-20 12:22:31
+
+ 
+
+?>
